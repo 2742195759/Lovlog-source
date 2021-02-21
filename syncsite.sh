4c852474e4e97250cdd1398dc12c311fc80cfdfc
xiongkun <xk18@mails.tsinghua.edu.cn>
2021-02-21 18:20:25 +0800

    fixbugs

--- syncsite.sh
+++ syncsite.sh
@@ -2,10 +2,13 @@ git add .
 git commit -m "fixbugs"
 git push origin master
 
+rm -r _site
 bundle exec jekyll build
 cp .nojekyll ./_site/
 cd ./_site
+git init
 git add .
-git commit -m "fixbugs"
-git push origin master
-
+git commit -m "lovlog"
+git branch -M master
+git remote add origin https://github.com/2742195759/Lovlog
+git push -f origin master
